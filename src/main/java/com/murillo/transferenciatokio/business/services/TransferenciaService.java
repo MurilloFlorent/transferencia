bd65fcadd977492a7e13bcbdd8d5e717abac9582
Murillo Florentino <murillo.florentin1@gmail.com>
2025-02-19 19:16:23 -0300

    Criando arquitetura de camadas

---
+++ src/main/java/com/murillo/transferenciatokio/business/services/TransferenciaService.java
@@ -0,0 +1,11 @@
+package com.murillo.transferenciatokio.business.services;
+
+import com.murillo.transferenciatokio.infrastructure.entities.Transferencia;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TransferenciaService {
+
+}
---
+++ src/main/java/com/murillo/transferenciatokio/controller/TransferenciaController.java
@@ -0,0 +1,17 @@
+package com.murillo.transferenciatokio.controller;
+
+import com.murillo.transferenciatokio.business.services.TransferenciaService;
+import com.murillo.transferenciatokio.infrastructure.entities.Transferencia;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/transferencia")
+public class TransferenciaController {
+
+
+}
